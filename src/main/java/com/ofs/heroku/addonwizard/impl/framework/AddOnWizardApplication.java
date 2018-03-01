db10c93ff3c3f8669914a45228b0a1870ab42afa
ganehsramr <mailganesh@gmail.com>
2018-03-01 10:18:35 -0500

    boomerang fix

--- src/main/java/com/ofs/heroku/addonwizard/impl/framework/AddOnWizardApplication.java
+++ src/main/java/com/ofs/heroku/addonwizard/impl/framework/AddOnWizardApplication.java
@@ -6,20 +6,20 @@ import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-public class BenchmarkApplication extends ResourceConfig {
+public class AddOnWizardApplication extends ResourceConfig {
 	
 	/**
      * Sets up all of the standard features.
      */
     @Inject
-    public BenchmarkApplication(final ServiceLocator serviceLocator) {
+    public AddOnWizardApplication(final ServiceLocator serviceLocator) {
     	
     	new JerseyHk2AnnotationScanner(serviceLocator).scan();
         register(new ApplicationBinder());
         register(io.swagger.jaxrs.listing.ApiListingResource.class);
         register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
         register(JacksonFeature.class);
-        packages(true, "com.acme.reference.impl.rest");
+        packages(true, "com.ofs.heroku.addonwizard.impl.rest");
     }
 }
 
